e31c136ef0d910967279bfe80d0ff6bb78ffe173
sato <sato_m@mediaxis.jp>
2020-03-10 21:03:29 +0900

    ユーザ一覧取得APIのテストケースを追加

--- handler/user_test.go
+++ handler/user_test.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,13 +13,25 @@ func TestUsers_ユーザ一覧が取得できること(t *testing.T) {
 	setup()
 	defer tearDown()
 
-	req := newRequest(http.MethodGet, "/api/v1/users", nil)
-	rec := httptest.NewRecorder()
-	c := newEchoContext(req, rec)
+	testcases := []struct {
+		name           string
+		query          string
+		goldenFilePath string
+	}{
+		{name: "クエリなし", query: "", goldenFilePath: "./testdata/user/users.golden"},
+		{name: "limitで取得件数指定", query: "limit=2", goldenFilePath: "./testdata/user/users_with_limit.golden"},
+		{name: "min_idで取得する最小のユーザIDを指定", query: "min_id=2", goldenFilePath: "./testdata/user/users_with_min_id.golden"},
+	}
 
-	assert.NoError(t, h.Users(c))
-	assertResponse(t, rec.Result(), 200, "./testdata/user/users.golden")
-	var r usersResponse
-	err := json.Unmarshal(rec.Body.Bytes(), &r)
-	assert.NoError(t, err)
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			target := fmt.Sprintf("%v?%v", "/api/v1/users", tc.query)
+			req := newRequest(http.MethodGet, target, nil)
+			rec := httptest.NewRecorder()
+			c := newEchoContext(req, rec)
+
+			assert.NoError(t, h.Users(c))
+			assertResponse(t, rec.Result(), 200, tc.goldenFilePath)
+		})
+	}
 }
